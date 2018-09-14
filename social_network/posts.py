e9826ccb0a92aff581baa473dea17ae3c8d22d27
Trace Tessier <tessiert@unm.edu>
2018-09-14 19:15:06 +0000

    test_posts passing

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,57 @@
-from datetime import datetime
+from datetime import datetime as dt
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text.lstrip('text=')
+        self.timestamp = timestamp
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):
+
+    def __str__(self):
+        return '@{first} {last}: "{text}"\n\t{timestamp}'.format(
+            first = self.user.first_name,
+            last = self.user.last_name,
+            text = self.text,
+            timestamp = self.timestamp.strftime('%A, %b %d, %Y'))
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        self.text = text.lstrip('text=')
+        self.url = image_url
+        self.timestamp = timestamp
+        self.user = None
+
+    def __str__(self):
+        return '@{first} {last}: "{text}"\n\t{url}\n\t{timestamp}'.format(
+            first = self.user.first_name,
+            last = self.user.last_name,
+            url = self.url,
+            text = self.text,
+            timestamp = self.timestamp.strftime('%A, %b %d, %Y'))
+
+
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        self.text = text.lstrip('text=')
+        self.lat = latitude
+        self.lon = longitude
+        self.timestamp = timestamp
+        self.user = None
+
+    def __str__(self):
+        coordinates = '{lat}, {lon}'.format(lat=self.lat, lon=self.lon)
+        print(coordinates)
+        return '@{first} Checked In: "{text}"\n\t{lat_lon}\n\t{timestamp}'.format(
+            first = self.user.first_name,
+            text = self.text,
+            lat_lon = coordinates,
+            timestamp = self.timestamp.strftime('%A, %b %d, %Y'))
